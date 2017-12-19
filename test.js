a423e9c04b32602438f34ad807c263e4cee932b2
Pablo Varela <pablovarela182@gmail.com>
2017-12-19 20:28:22 +0100

    Fix tests

--- test.js
+++ test.js
@@ -4,25 +4,22 @@ const { shellSync } = require('execa')
 const exe = shellSync
 
 test('returns a value greater than 0', async t => {
-  const { stdout, stderr } = exe(`./index.js`, { cwd: __dirname })
+  const { stdout, code } = exe(`./index.js`, { cwd: __dirname })
   const result = parseFloat(stdout.trim())
-  t.falsy(stderr)
+  t.is(code, 0)
   t.true(result > 0.0)
-  console.log(stdout)
 })
 
 test('ends with %', async t => {
-  const { stdout, stderr } = exe(`./index.js`, { cwd: __dirname })
+  const { stdout, code } = exe(`./index.js`, { cwd: __dirname })
   const result = stdout.trim()[stdout.length - 1]
-  t.falsy(stderr)
+  t.is(code, 0)
   t.is(result, '%')
-  console.log(stdout)
 })
 
 test('has two decimals', async t => {
-  const { stdout, stderr } = exe(`./index.js`, { cwd: __dirname })
+  const { stdout, code } = exe(`./index.js`, { cwd: __dirname })
   const decimals = stdout.trim().split('%')[0].split('.')[1]
-  t.falsy(stderr)
+  t.is(code, 0)
   t.is(decimals.length, 2)
-  console.log(stdout)
 })
