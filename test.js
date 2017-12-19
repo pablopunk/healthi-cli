5d3fc58ad8c206ad5dcf540523fce741059814dd
Pablo Varela <pablovarela182@gmail.com>
2017-12-19 20:57:36 +0100

    0-100

--- test.js
+++ test.js
@@ -3,10 +3,11 @@ const { shellSync } = require('execa')
 
 const exe = shellSync
 
-test('returns a value greater than 0', async t => {
+test('returns a value between 0 and 100', async t => {
   const { stdout } = exe(`./index.js`, { cwd: __dirname })
   const result = parseFloat(stdout.trim())
   t.true(result > 0.0)
+  t.true(result <= 100.0)
 })
 
 test('ends with %', async t => {
