b968864a38e2a3f7154cff2a6bad9c28a49bcbff
pablopunk <pablovarela182@gmail.com>
2019-01-21 10:53:31 +0100

    Run on osx

--- test.js
+++ test.js
@@ -4,12 +4,10 @@ const { shellSync } = require('execa')
 const exe = shellSync
 
 test('returns a value between 0 and 100', async t => {
-  try {
-    const { stdout } = exe(`./index.js`, { cwd: __dirname })
-    const result = parseFloat(stdout.trim())
-    t.true(result > 0.0)
-    t.true(result <= 100.0)
-  } catch (err) {}
+  const { stdout } = exe(`./index.js`, { cwd: __dirname })
+  const result = parseFloat(stdout.trim())
+  t.true(result > 0.0)
+  t.true(result <= 100.0)
 })
 
 test('ends with %', async t => {
